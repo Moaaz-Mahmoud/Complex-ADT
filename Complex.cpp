cbc61dd239371e2d1a2870c58283813f2406b34a
Moaaz-Mahmoud <61514465+Moaaz-Mahmoud@users.noreply.github.com>
2020-06-01 15:04:02 +0200

    Update Complex.cpp

--- Complex.cpp
+++ Complex.cpp
@@ -1,215 +1,185 @@
-/* Complex.cpp
-   Implements the Complex class.
+/* The Complex class.cpp
+   The driver for the Complex library.
 */
+
 #include "Complex.h"
+#define TESTING 0		//Set this flag to 1 to enable testing
+						//or to 0 to disable.
 
-using std::cin; using std::cout;
-using std::string;
-using std::istream; using std::ostream;
+Complex z1(-3, -4), z2, z3;
+//Unit testing.
+#if TESTING	
+void testBasic();
+void testArith_Com_typ();
+void testArith_typ_Com();
+void testRelational();
+void testPowRt();
+void testExcept();
+void testform();
+//Integration testing.
+void IntegrationTesting();
+#endif
 
-//Basic member functions.
-void  Complex::get() {
-	cin >> real >> imaginary;
-}
-float Complex::get_real() {
-	return real;
-}
-float Complex::get_imaginary() {
-	return imaginary;
-}
-void  Complex::set(float x, float y, form_t c) {
-	real = x;
-	imaginary = y;
-}
-void  Complex::set_form(form_t f) {
-	form = f;
-}
-void  Complex::showREC()		 const {
-	cout << '(';
-	if (real > 0)
-		if (imaginary > 0)	//_________________________a + bi
-			cout << real << " + " << imaginary << 'i';
-		else if (imaginary == 0)	//_____________________a
-			cout << real;
-		else	//_____________________________________a-bi
-			cout << real << " - " << -imaginary << 'i';
-	else if (real == 0)
-		if (imaginary > 0)	//_________________________bi
-			cout << imaginary << 'i';
-		else if (imaginary == 0)	//_____________________0
-			cout << '0';
-		else	//_____________________________________-bi
-			cout << imaginary << 'i';
-	else //real < 0
-		if (imaginary > 0)	//_________________________-a+bi
-			cout << real << " + " << imaginary << 'i';
-		else if (imaginary == 0)	//_____________________-a
-			cout << real;
-		else	//_____________________________________-a-bi
-			cout << real << " - " << -imaginary << 'i';
-	cout << ')';
-}
-void  Complex::showPOL()		 const {
-	float r = sqrt(pow(real, 2) + pow(imaginary, 2));
-	float theta;
-	try {
-		theta = this->calculate_angle();
-	}
-	catch (Complex::unspec_ang) {
-		theta = 0;
-		goto a;
-	}
-a: std::cout << "(" << r << ", " << theta << ")";
-}
-void  Complex::showEXP()		 const {
-	float r = sqrt(pow(real, 2) + pow(imaginary, 2));
-	float theta;
-	try {
-		theta = this->calculate_angle();
-	}
-	catch (Complex::unspec_ang) {
-		theta = 0;
-		goto b;
-	}
-b: std::cout << "(" << r << "e^(" << theta << "i))";
-}
-Complex Complex::divide_by(Complex c) {
-	Complex quotient;
-	float den;		//common denominator c^2 + d^2
-	if (c.get_real() == 0 && c.get_imaginary() == 0)
-		throw Complex::zero_div();
-	den = pow(c.real, 2) + pow(c.imaginary, 2);
-	quotient.real = (real * c.real + imaginary * c.imaginary) / den;
-	quotient.imaginary = (imaginary * c.real - real * c.imaginary) / den;
-	quotient.set_form(form);
-	return quotient;
-}
-float Complex::calculate_angle() const {
-	if (real > 0)									//////
-		if (imaginary > 0)								//////
-			return atan(imaginary / real);				//////
-		else if (!imaginary)								//////
-			return 0;									//////
-		else											//////
-			return -atan(abs(imaginary) / abs(real));	//////
-	else if (!real)							//////
-		if (imaginary > 0)					//////
-			return PI / 2;					//////
-		else if (!imaginary)					//////
-			throw Complex::unspec_ang();	//////
-		else								//////
-			return 3 * PI / 2;					//////
-	else													//////
-		if (imaginary > 0)									//////
-			return PI - atan(abs(imaginary) / abs(real));	//////
-		else if (!imaginary)									//////
-			return PI;										//////
-		else												//////
-			return PI + atan(abs(imaginary) / abs(real));	//////
-}
-float Complex::rad_angle()       const {
-	try {
-		return this->calculate_angle();
-	}
-	catch (Complex::unspec_ang) {}
-}
-float Complex::deg_angle()       const {
-	return (180 / PI) * this->rad_angle();
-}
-float Complex::get_magnitude()   const {
-	return sqrt(pow(real, 2) + pow(imaginary, 2));
-}
-Complex Complex::conjugate()     const {
-	Complex con = *this;
-	con.imaginary = -(con.imaginary);
-	con.set_form(form);
-	return con;
-}
-//Complex operators (Complex with Complex).
-Complex Complex::operator + (Complex c)
+int main()
 {
-	Complex sum;
-	sum.real = real + c.real;
-	sum.imaginary = imaginary + c.imaginary;
-	sum.set_form(form);
-	return sum;
+#if TESTING
+	printf_s("  %c  Unit testing\n\n\n", 254);
+	testBasic();
+	testArith_Com_typ();
+	testArith_typ_Com();
+	testRelational();
+	testPowRt();
+	testExcept();
+	testform();
+
+	std::cout
+		<< "\n\n*********************************************\n\n\n\n\n";
+
+	printf_s("  %c  Integration testing\n\n\n", 254);
+	IntegrationTesting();
+#endif
+
+	system("pause"); return 0;
 }
-Complex Complex::operator - (Complex c)
-{
-	Complex difference;
-	difference.real = real - c.real;
-	difference.imaginary = imaginary - c.imaginary;
-	difference.set_form(form);
-	return difference;
+
+#if TESTING
+void testBasic() {
+	std::cout
+		<< ' ' << char(254) << ' '
+		<< "Basic operations( Printing, reading"
+		<< "and arithmetic(Complex with Complex) ) :-\n\n";
+	std::cout << ">>> "; std::cin >> z2;
+	z3 = z1 + z2;
+	z3 = z1 + z2; std::cout << "z1 + z2 = " << z3 << '\n';
+	z3 = z1 - z2;
+	std::cout << "z1 - z2 = " << z3 << '\n';
+	z3 = z1 * z2;
+	std::cout << "z1 * z2 = " << z3 << '\n';
+	z3 = z1 / z2;
+	std::cout << "z1 / z2 = " << z3 << '\n';
+	z3 = z1.conjugate();
+	std::cout
+		<< "z1 radian angle : " << z1.rad_angle() << '\n'
+		<< "z1 degree angle : " << z1.deg_angle() << '\n'
+		<< "z1 magnitude    : " << z1.get_magnitude() << '\n'
+		<< "z1 conjugate    : " << z3 << "\n\n\n";
 }
-Complex Complex::operator * (Complex c)
-{
-	Complex product;
-	product.real = real * c.real -
-		imaginary * c.imaginary;
-	product.imaginary = real *
-		c.imaginary + imaginary * c.real;
-	product.set_form(form);
-	return product;
+void testArith_Com_typ() {
+	std::cout
+		<< ' ' << char(254) << ' '
+		<< "Arithmetic operators(Complex with othe datatypes) :-\n\n";
+	z3 = z1 + 2;
+	std::cout << "z1 + 2 = " << z3 << '\n';
+	z3 = z1 - 2;
+	std::cout << "z1 - 2 = " << z3 << '\n';
+	z3 = z1 * 2;
+	std::cout << "z1 * 2 = " << z3 << '\n';
+	z3 = z1 / 2;
+	std::cout << "z1 / 2 = " << z3 << "\n\n\n";
 }
-Complex Complex::operator / (Complex c)
-{
-	Complex quotient;
+void testArith_typ_Com() {
+	std::cout
+		<< ' ' << char(254) << ' '
+		<< "Arithmetic operators(Other datatypes with Complex) :-\n\n";
+	z3 = 4 + z1;
+	std::cout << "4 + z1 = " << z3 << '\n';
+	z3 = 4 - z1;
+	std::cout << "4 - z1 = " << z3 << '\n';
+	z3 = 4 * z1;
+	std::cout << "4 * z1 = " << z3 << '\n';
+	z3 = 4 / z1;
+	std::cout << "4 / z1 = " << z3 << "\n\n\n";
+}
+void testRelational() {
+	std::cout
+		<< ' ' << char(254) << ' '
+		<< "Relational operators :-\n\n";
+	z1.set(3.3, 4.4);
+	z2.set(3.3, 4.4);
+	z3.set(2, 2);
+	if (z1 == z2)
+		std::cout << "z1 and z2 are equal\n";
+	else
+		std::cout << "z1 and z2 are not equal\n";
+	if (z1 == z3)
+		std::cout << "z1 and z3 are equal";
+	else
+		std::cout << "z1 and z3 are not equal";
+	std::cout << "\n\n\n";
+}
+void testPowRt() {
+	std::cout
+		<< ' ' << char(254) << ' '
+		<< "Power and roots :-\n\n";
+	z3 = z1.power(4);
+	std::cout
+		<< "z1 ^ 4 = " << z3 << '\n'
+		<< "z1 4th roots:-\n";
+	Complex* ptr = z1.root(4);
+	for (int i = 0; i < 4; i++)
+		std::cout << *ptr++ << '\n';
+	std::cout << "\n\n";
+}
+void testExcept() {
+	std::cout
+		<< ' ' << char(254) << ' '
+		<< "Exception classes :-\n"
+		<< char(254) << "The exception class zero_div\n"
+		<< "__Should be thrown only once__.\n\n";
+	// Should be thrown here.
 	try {
-		quotient = this->divide_by(c);
+		z3 = z1 / 0;
+		std::cout << "z3 = " << z3 << "\n\n\n";
 	}
-	catch (Complex::zero_div()) {}
-	return quotient;
-}
-//Complex operators (Complex with other datatypes) 
-//  --> Defined in the header.
-//Operators (Other datatypes with Complex) 
-//  --> Defined in the header.
-//Insertion '<<' and extraction '>>' operators.
-istream& operator >> (istream& s, Complex& C) {
-	C.get();
-	return s;
-}
-ostream& operator << (ostream& s, Complex& C) {
-	switch (C.getform()) {
-	case REC: C.showREC(); break;
-	case POL: C.showPOL(); break;
-	case EXP: C.showEXP(); break;
+	catch (Complex::zero_div) {}
+	// Shouldn't be thrown here.
+	try {
+		z3 = z1 / 2;
+		std::cout << "z3 = " << z3 << "\n\n\n";
 	}
-	return s;
+	catch (Complex::zero_div) {}
+
+	Complex z0(0, 0);
+	std::cout << z0.rad_angle() << '\n';
+	std::cout << z0.deg_angle() << '\n';
+	z0.set(0, 1);
+	std::cout << z0.rad_angle();
+	std::cout << z0.deg_angle();
+
+	z3.set(0, 4, POL);
+	std::cout << "z3_pol = " << z3 << '\n';
+	z3.set(0, 0);
+	std::cout << "zero pol = " << z3 << '\n';
+	z3.set_form(EXP);
+	std::cout << "zero exp = " << z3 << '\n';
+	std::cout << "\n\n";
 }
-//Relational operators ('==' and '!=')
-bool Complex::operator == (Complex C) {
-	return (real == C.get_real()) &&
-		(imaginary == C.get_imaginary());
+void testform() {
+	std::cout
+		<< ' ' << char(254) << ' '
+		<< "Form :-\n\n";
+	z1.set_form(POL);
+	std::cout << "z1_pol = " << z1 << '\n';
+	z1.set_form(EXP);
+	std::cout << "z1_exp = " << z1 << '\n';
 }
-bool Complex::operator != (Complex C) {
-	return !((*this) == C);
+
+void IntegrationTesting() {
+	Complex* ptr;
+	z3 = z1 + z2 - 4 * z2 / z1;
+	std::cout << "z1 + z2 - 4 * z2 / z1" << z3 << '\n';
+	ptr = (z1 + 2 * z2).root(3);
+	std::cout << "The cubic roots of z1 + 2 * z2 :-\n";
+	for (int i = 0; i < 2; i++)
+		std::cout << *ptr++ << '\n';
+	std::cout << (z1 + 2 * z2).root(3)[2] << '\n';
+	std::cout 
+		<< "z1 rad angle : " 
+		<< z1.conjugate().conjugate().rad_angle() << '\n';
+	z3 = (z2 = z1 + 2) + 2;
+	std::cout 
+		<< "z1 : " << z1 << "\tz2 : " 
+		<< z2 << "\tz3 : " << z3 << '\n';
 }
-//Power and roots.
-Complex Complex::power(int n) {
-	Complex result;
-	float r = pow(this->get_magnitude(), n);
-	float theta = this->rad_angle() * n;
-	result.set(r * cos(theta), r * sin(theta));
-	result.set_form(form);
-	return result;
-}
-Complex* Complex::root(const int k) {
-	Complex* results;
-	results = new Complex[k];
-	for (int i = 0; i < k; i++) {
-		results[i].set(
-			pow(this->get_magnitude(),
-				1 / static_cast<float>(k)) *
-				cos((this->rad_angle() + 2 *
-				PI * (i - 1)) / static_cast<float>(k)),
-			pow(this->get_magnitude(),
-				1 / static_cast<float>(k)) *
-			sin((this->rad_angle() + 2 * PI *
-			(i - 1)) / static_cast<float>(k))
-		);
-		results[i].set_form(form);
-	}
-	return results;
-}
+
+#endif
