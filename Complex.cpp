7e79083183a58d15fb6b3f6e0396973428c89a52
Moaaz-Mahmoud <61514465+Moaaz-Mahmoud@users.noreply.github.com>
2020-06-01 15:03:22 +0200

    Update Complex.cpp

--- Complex.cpp
+++ Complex.cpp
@@ -1,7 +1,6 @@
 /* Complex.cpp
    Implements the Complex class.
 */
-#include "stdafx.h"
 #include "Complex.h"
 
 using std::cin; using std::cout;
@@ -22,7 +21,7 @@ void  Complex::set(float x, float y, form_t c) {
 	real = x;
 	imaginary = y;
 }
-void  Complex::set_form(form_t f){ 
+void  Complex::set_form(form_t f) {
 	form = f;
 }
 void  Complex::showREC()		 const {
@@ -50,29 +49,29 @@ void  Complex::showREC()		 const {
 			cout << real << " - " << -imaginary << 'i';
 	cout << ')';
 }
-void  Complex::showPOL()		 const{
-	float r = sqrt( pow(real, 2) + pow(imaginary, 2) );
+void  Complex::showPOL()		 const {
+	float r = sqrt(pow(real, 2) + pow(imaginary, 2));
 	float theta;
 	try {
-	theta = this->calculate_angle();
+		theta = this->calculate_angle();
 	}
 	catch (Complex::unspec_ang) {
 		theta = 0;
 		goto a;
 	}
-	a: std::cout << "(" << r << ", " << theta << ")";
+a: std::cout << "(" << r << ", " << theta << ")";
 }
-void  Complex::showEXP()		 const{
-	float r = sqrt( pow(real, 2) + pow(imaginary, 2) );
+void  Complex::showEXP()		 const {
+	float r = sqrt(pow(real, 2) + pow(imaginary, 2));
 	float theta;
 	try {
-	theta = this->calculate_angle();
+		theta = this->calculate_angle();
 	}
 	catch (Complex::unspec_ang) {
 		theta = 0;
 		goto b;
 	}
-	b: std::cout << "(" << r << "e^(" << theta << "i))";
+b: std::cout << "(" << r << "e^(" << theta << "i))";
 }
 Complex Complex::divide_by(Complex c) {
 	Complex quotient;
@@ -85,25 +84,25 @@ Complex Complex::divide_by(Complex c) {
 	quotient.set_form(form);
 	return quotient;
 }
-float Complex::calculate_angle() const{
-		if(real > 0)									//////
-		if(imaginary > 0)								//////
+float Complex::calculate_angle() const {
+	if (real > 0)									//////
+		if (imaginary > 0)								//////
 			return atan(imaginary / real);				//////
-		else if(!imaginary)								//////
+		else if (!imaginary)								//////
 			return 0;									//////
 		else											//////
 			return -atan(abs(imaginary) / abs(real));	//////
-	else if(!real)							//////
-		if(imaginary > 0)					//////
-			return PI/2;					//////
-		else if(!imaginary)					//////
+	else if (!real)							//////
+		if (imaginary > 0)					//////
+			return PI / 2;					//////
+		else if (!imaginary)					//////
 			throw Complex::unspec_ang();	//////
 		else								//////
-			return 3*PI/2;					//////
+			return 3 * PI / 2;					//////
 	else													//////
-		if(imaginary > 0)									//////
+		if (imaginary > 0)									//////
 			return PI - atan(abs(imaginary) / abs(real));	//////
-		else if(!imaginary)									//////
+		else if (!imaginary)									//////
 			return PI;										//////
 		else												//////
 			return PI + atan(abs(imaginary) / abs(real));	//////
@@ -115,7 +114,7 @@ float Complex::rad_angle()       const {
 	catch (Complex::unspec_ang) {}
 }
 float Complex::deg_angle()       const {
-	return (180/PI) * this->rad_angle();
+	return (180 / PI) * this->rad_angle();
 }
 float Complex::get_magnitude()   const {
 	return sqrt(pow(real, 2) + pow(imaginary, 2));
@@ -146,29 +145,33 @@ Complex Complex::operator - (Complex c)
 Complex Complex::operator * (Complex c)
 {
 	Complex product;
-	product.real = real * c.real - imaginary * c.imaginary;
-	product.imaginary = real * c.imaginary + imaginary * c.real;
+	product.real = real * c.real -
+		imaginary * c.imaginary;
+	product.imaginary = real *
+		c.imaginary + imaginary * c.real;
 	product.set_form(form);
 	return product;
 }
 Complex Complex::operator / (Complex c)
 {
 	Complex quotient;
-	try{
+	try {
 		quotient = this->divide_by(c);
 	}
-	catch(Complex::zero_div()){}
+	catch (Complex::zero_div()) {}
 	return quotient;
 }
-//Complex operators (Complex with other datatypes) --> Defined in the header.
-//Operators (Other datatypes with Complex) ----------> Defined in the header.
+//Complex operators (Complex with other datatypes) 
+//  --> Defined in the header.
+//Operators (Other datatypes with Complex) 
+//  --> Defined in the header.
 //Insertion '<<' and extraction '>>' operators.
-istream & operator >> (istream& s, Complex& C) {
+istream& operator >> (istream& s, Complex& C) {
 	C.get();
 	return s;
 }
 ostream& operator << (ostream& s, Complex& C) {
-	switch(C.form) {
+	switch (C.getform()) {
 	case REC: C.showREC(); break;
 	case POL: C.showPOL(); break;
 	case EXP: C.showEXP(); break;
@@ -177,7 +180,8 @@ ostream& operator << (ostream& s, Complex& C) {
 }
 //Relational operators ('==' and '!=')
 bool Complex::operator == (Complex C) {
-	return (real == C.get_real()) && (imaginary == C.get_imaginary());
+	return (real == C.get_real()) &&
+		(imaginary == C.get_imaginary());
 }
 bool Complex::operator != (Complex C) {
 	return !((*this) == C);
@@ -191,13 +195,19 @@ Complex Complex::power(int n) {
 	result.set_form(form);
 	return result;
 }
-Complex *Complex::root(const int k) {
+Complex* Complex::root(const int k) {
 	Complex* results;
 	results = new Complex[k];
 	for (int i = 0; i < k; i++) {
 		results[i].set(
-			pow(this->get_magnitude(), 1 / static_cast<float>(k))* cos((this->rad_angle() + 2 * PI * (i - 1)) / static_cast<float>(k)),
-			pow(this->get_magnitude(), 1 / static_cast<float>(k))* sin((this->rad_angle() + 2 * PI * (i - 1)) / static_cast<float>(k))
+			pow(this->get_magnitude(),
+				1 / static_cast<float>(k)) *
+				cos((this->rad_angle() + 2 *
+				PI * (i - 1)) / static_cast<float>(k)),
+			pow(this->get_magnitude(),
+				1 / static_cast<float>(k)) *
+			sin((this->rad_angle() + 2 * PI *
+			(i - 1)) / static_cast<float>(k))
 		);
 		results[i].set_form(form);
 	}
