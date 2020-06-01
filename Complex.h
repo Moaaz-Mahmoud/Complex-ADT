cc452c68688af248aa498eb25a7bf591447f322b
Moaaz-Mahmoud <61514465+Moaaz-Mahmoud@users.noreply.github.com>
2020-06-01 15:01:48 +0200

    Update Complex.h

--- Complex.h
+++ Complex.h
@@ -1,13 +1,20 @@
+/* Complex.h
+   The header file that contains the
+   definition of the Complex class.
+*/
+
 #ifndef COMPLEX_H_
 #define COMPLEX_H_
 
-#include "StdAfx.h"
-#include "Complex.h"
+#include <cstdlib>
 #include <iostream>
+#include <string>
 #include <cmath>
+#include <stdio.h>
+#include <stdlib.h>
 const float PI = 3.14159;
 
-enum form_t {REC, POL, EXP};
+enum form_t { REC, POL, EXP };
 class Complex
 {
 	//Data members.
@@ -33,6 +40,7 @@ public:
 	void  set_form(form_t c);
 	float get_real();
 	float get_imaginary();
+	form_t getform() { return form; }
 	//Basic member functions
 	float rad_angle()	  const;
 	float deg_angle()	  const;
@@ -49,73 +57,73 @@ public:
 	//Operators (Complex with other datatypes)
 	template <class T>
 	Complex operator + (T num) {
-	Complex sum;
-	sum.real = real + num;
-	sum.imaginary = imaginary;
-	sum.set_form(form);
-	return sum;
+		Complex sum;
+		sum.real = real + num;
+		sum.imaginary = imaginary;
+		sum.set_form(form);
+		return sum;
 	}
 	template <class T>
 	Complex operator - (T num) {
-	Complex difference;
-	difference.real = real - num;
-	difference.imaginary = imaginary;
-	difference.set_form(form);
-	return difference;
+		Complex difference;
+		difference.real = real - num;
+		difference.imaginary = imaginary;
+		difference.set_form(form);
+		return difference;
 	}
 	template <class T>
 	Complex operator * (T num) {
-	Complex product;
-	product.real = real * num;
-	product.imaginary = imaginary * num;
-	product.set_form(form);
-	return product;
-}
+		Complex product;
+		product.real = real * num;
+		product.imaginary = imaginary * num;
+		product.set_form(form);
+		return product;
+	}
 	template <class T>
 	Complex operator / (T num) {
-		Complex quotient(0,0);
+		Complex quotient(0, 0);
 		Complex Num(num, 0);
-		try{
+		try {
 			quotient = this->divide_by(Num);
 		}
-		catch(Complex::zero_div()) {}
+		catch (Complex::zero_div()) {}
 		return quotient;
 	}
 	//Operators (Other datatypes with Complex)
 	template <class T>
-	friend Complex operator + (T num, Complex C){
-	return C + num;
+	friend Complex operator + (T num, Complex C) {
+		return C + num;
 	}
 	template <class T>
 	friend Complex operator - (T num, Complex C) {
-	return (C - num) * (-1);
+		return (C - num) * (-1);
 	}
 	template <class T>
 	friend Complex operator * (T num, Complex C) {
-	return C * num;
+		return C * num;
 	}
 	template <class T>
 	friend Complex operator / (T num, Complex C) {
-	float x, y;
-	Complex pre_result;
-	if (C.get_real() == 0 && C.get_imaginary() == 0)
-		throw Complex::zero_div();
-	x = C.get_real() / (pow(C.get_real(), 2) + pow(C.get_imaginary(), 2));
-	y = -C.get_imaginary() / (pow(C.get_real(), 2) + pow(C.get_imaginary(), 2));
-	pre_result.set(x, y);
-	return num * pre_result;
+		float x, y;
+		Complex pre_result;
+		if (C.get_real() == 0 && C.get_imaginary() == 0)
+			throw Complex::zero_div();
+		x = C.get_real() / (pow(C.get_real(), 2) + pow(C.get_imaginary(), 2));
+		y = -C.get_imaginary() / (pow(C.get_real(), 2) + pow(C.get_imaginary(), 2));
+		pre_result.set(x, y);
+		return num * pre_result;
 	}
 	//Relational operators ('==' and '!=')
 	bool operator == (Complex C);
 	bool operator != (Complex C);
 	//Power and roots
 	Complex power(int n);
-	Complex *root(const int k);
+	Complex* root(const int k);
 	//Exception classes
 	class zero_div {
 	public:
 		zero_div() {
-			std::cout 
+			std::cout
 				<< " *** An exception was thrown due to an attempt\n"
 				<< "     to divide by zero.\n"
 				<< " *** Continue anyway? 1:cont, else: exit\n";
